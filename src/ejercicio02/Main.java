6127aaef424d636665ed9ec60a786d30314eb672
soyne <soyne@MSI-NESTOR>
2023-11-21 17:34:26 +0100

    Recommit Ejercicio 02 Tema 3 NO ENTREGABLE

--- src/ejercicio02/Main.java
+++ src/ejercicio02/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int numeroA; 
-		int numeroB;
+		int numeroA=0; 
+		int numeroB=0;
 		
 		// Inicia el Scanner
 		Scanner sc = new Scanner(System.in);
@@ -19,17 +19,18 @@ public class Main {
 			System.out.println("Inserta un numero");
 			numeroA = sc.nextInt();
 		} catch (InputMismatchException e) {
-			System.out.println("ERROR: El valor introducido no es un numero");
+			System.out.println("ERROR: El valor introducido no es un numero. El valor se establecera en 0.");
 		}
 		
 		try {
 			System.out.println("Inserta otro numero");
 			numeroB = sc.nextInt();
 		} catch (InputMismatchException e) {
-			System.out.println("ERROR: El valor introducido no es un numero");
+			System.out.println("ERROR: El valor introducido no es un numero. El valor se establecera en 0.");
 		}
 		
 		// Aqui ira la continuacion del codigo
+		Numeros.numeros(numeroA, numeroB);
 		
 		// Cierra el Scanner
 		sc.close();
